9fccb630fcd4771d40c9cd54beb4523015a1e4f2
Chijioke <8pluscreative@gmail.com>
2022-01-20 22:18:24 -0800

    Linking pages

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,26 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+	{id: 'q1', author: 'CJ', text: 'React is Interesting!'},
+	{id: 'q2', author: 'Chika', text: 'Reactjs is great!'}
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+	const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+	if (!quote) {
+		return <p>No quote found!</p>
+	}
 	
 	return (
 		<Fragment>
-			<h1>Quote Detail Page</h1>
-			<p>{params.quoteId}</p>
+			<HighlightedQuote text={quote.text} author={quote.author}/>
 			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
