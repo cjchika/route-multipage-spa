79ce22a47ec2ca1d0fb7f6e25f5d883b585ae553
Chijioke <8pluscreative@gmail.com>
2022-01-21 19:57:58 -0800

    Fetching Comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,15 +1,44 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.development';
 
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { quoteId } = params;
+  
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(params);
+  }, [quoteId, sendRequest])
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
   
+  const onAddedCommentHandler = () => {
+
+  };
+
+  let comments;
+
+  if (status === 'pending') {
+    comments = <div className='centered'><LoadingSpinner /></div>
+  }
+
+  if (status === 'completed' && loadedComments) {
+    comments = <CommentsList />
+  }
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,7 +47,7 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
+      {isAddingComment && <NewCommentForm quoteId={params.quoteId} onAddedComment={onAddedCommentHandler} />}
       <p>Comments...</p>
     </section>
   );
